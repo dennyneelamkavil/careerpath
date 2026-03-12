72765356bc3127aa29a6c6a3f4599e2505ab8214
Denny <dennynj4748@gmail.com>
2026-03-12 18:24:45 +0530

    adapting components, nextauth , & other files from similar project repo

---
+++ lib/types/index.ts
@@ -0,0 +1,18 @@
+export type {
+  Media,
+  TempMediaFolder,
+  FinalMediaFolder,
+  MediaFolder,
+} from "./media";
+
+export type { PageSeo } from "./page-seo";
+
+export type { PaginationMeta } from "./pagination";
+
+export type { Permission, PermissionBase } from "./permission";
+
+export type { Role, RoleBase, RoleWithPermissionKeys } from "./role";
+
+export type { Seo } from "./seo";
+
+export type { User, UserBase } from "./user";
